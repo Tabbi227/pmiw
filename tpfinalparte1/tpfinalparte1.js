6ccebeeac05a07845e16dded2c54b2d48b5cac72
Agustin Tabbita <36090595+Tabbi227@users.noreply.github.com>
2024-10-19 21:28:23 -0300

    Add files via upload

--- tpfinalparte1/tpfinalparte1.js
+++ tpfinalparte1/tpfinalparte1.js
@@ -1,10 +1,11 @@
-/* 
+/*
 
+ *Candela Di Lorenzo 120296/0
  *Agustin Tabbita 119151/7
- *Tp1
+ *Tp final - parte 1
  *Comision 1
  *Video Explicacion:
-    https:/
+   https://youtu.be/91NIUT8FKiU
 
 */
 
@@ -12,46 +13,50 @@ let imagenes = [];
 let imagenTexto0;
 let estado = 0;
 let textos = ["AXOLOTL",
-"Visité el jardín zoológico casi todas las tardes. Me gustaba mirar los axolotls en su estanque. Eran criaturas misteriosas, con ojos que parecían sonreír. Su mirada me hipnotizaba. Me sentía atraído por su mundo submarino. Quería entenderlos.",
-"Me senté frente al estanque y los observé. Sus movimientos eran lentos y gráciles. Parecían bailar en el agua. Sus ojos me miraban con curiosidad. Me sentí conectado a ellos. Quería saber más.",
-"Comencé a sentir una conexión emocional con los axolotls. Me imaginaba a mí mismo en su lugar. Sentía su tranquilidad y paz. Quería ser uno de ellos. Su mundo era más atractivo que el mío.",
-"Me imaginé que me convertía en un axolotl. Sentía el agua rodeándome. Mi respiración se volvió lenta y profunda. Mi visión se amplió. Veía el mundo desde una nueva perspectiva.",
-"Descubrí un mundo lleno de belleza y misterio. Las plantas acuáticas me rodeaban. Los peces nadaban a mi alrededor. Sentía la libertad de estar en el agua. Era parte del ecosistema.",
-"Intenté comunicarme con los axolotls. Pero no había palabras. Solo gestos y miradas. Sentí una profunda conexión. Entendí su lenguaje silencioso.",
-"Me identifiqué con los axolotls. Sentí su soledad y marginación. Comprendí su condición de criaturas olvidadas. Me sentí solidario con ellos. Eran mis hermanos.",
-"Me convertí en un axolotl. Mi cuerpo se transformó. Mis sentidos se ampliaron. Sentía el mundo de manera diferente. Era uno de ellos.",
-"Sentí una profunda liberación. Era libre de ser yo mismo. No había límites ni fronteras. Era parte del universo. Era uno con todo.",
-"Regresé a mi forma humana. Pero no era el mismo. Había cambiado. Había entendido algo nuevo. Me sentí renovado y transformado.",
-"Me convertí en un axolotl para siempre. Mi cuerpo se adaptó al agua. Mis sentidos se ampliaron. Sentía el mundo de manera diferente. Era uno de ellos.",
-"Viví entre los axolotls, aprendiendo su lenguaje. Compartí su comida y su refugio. Me convertí en parte del ecosistema. Sentía la armonía del universo.",
-"Descubrí que los axolotls eran inmortales. Su ciclo de vida se repetía eternamente. Me uní a ese ciclo, libre de preocupaciones.Era parte de la eternidad.",
-"Me fusioné con el universo, sin límites. Era uno con todo, sin distinciones.La conciencia se expandió, infinita. Era la unidad absoluta.",
-"Desperté en el jardín zoológico, confundido. Recordé mi experiencia como axolotl. Pero parecía un sueño lejano. Me sentí desconectado de la realidad.",
-"Reflexioné sobre mi experiencia. Comprendí la conexión entre humanos y animales. Sentí una nueva empatía hacia los axolotls. Me comprometí a protegerlos.",
-"Decidí compartir mi experiencia con otros. Escribí un libro sobre mi transformación. La gente se interesó en los axolotls. Se creó un movimiento para protegerlos.",
-"Mi historia inspiró a futuras generaciones. Los axolotls fueron protegidos y respetados. Dejé un legado de amor y comprensión. Mi transformación no fue en vano.",
-"FIN",
-"CREDITOS\n Candela Melino \n Agustin Tabbita"];
+  "Visité el jardín zoológico casi todas las tardes. Me gustaba mirar los axolotls en su estanque. Eran criaturas misteriosas, con ojos que parecían sonreír. Su mirada me hipnotizaba. Me sentía atraído por su mundo submarino. Quería entenderlos.",
+  "Me senté frente al estanque y los observé. Sus movimientos eran lentos y gráciles. Parecían bailar en el agua. Comencé a sentir una conexión emocional con los axolotls. Quería ser uno de ellos. Su mundo era más atractivo que el mío.",
+  "Me imaginé que me convertía en un axolotl. Sentía el agua rodeándome. Mi respiración se volvió lenta y profunda. Mi visión se amplió. Veía el mundo desde una nueva perspectiva. Descubrí un mundo lleno de belleza y misterio.",
+  "Las plantas acuáticas me rodeaban.",
+  "Los peces nadaban a mi alrededor.",
+  "Intenté comunicarme con los axolotls. Pero no había palabras. Solo gestos y miradas. Sentí una profunda conexión. Entendí su lenguaje silencioso.",
+  "Me identifiqué con los axolotls. Sentí su soledad y marginación. Comprendí su condición de criaturas olvidadas. Me sentí solidario con ellos. Eran mis hermanos.",
+  "Me convertí en un axolotl. Mi cuerpo se transformó. Mis sentidos se ampliaron. Sentía el mundo de manera diferente. Era uno de ellos.",
+  "Sentí una profunda liberación. Era libre de ser yo mismo. No había límites ni fronteras. Era parte del universo. Era uno con todo.",
+  "Regresé a mi forma humana. Pero no era el mismo. Había cambiado. Había entendido algo nuevo. Me sentí renovado y transformado.",
+  "Me convertí en un axolotl para siempre. Mi cuerpo se adaptó al agua. Mis sentidos se ampliaron. Sentía el mundo de manera diferente. Era uno de ellos.",
+  "Viví entre los axolotls, aprendiendo su lenguaje. Compartí su comida y su refugio. Me convertí en parte del ecosistema. Sentía la armonía del universo.",
+  "Descubrí que los axolotls eran inmortales. Su ciclo de vida se repetía eternamente. Me uní a ese ciclo, libre de preocupaciones.Era parte de la eternidad.",
+  "Me fusioné con el universo, sin límites. Era uno con todo, sin distinciones.La conciencia se expandió, infinita. Era la unidad absoluta.",
+  "Desperté en el jardín zoológico, confundido. Recordé mi experiencia como axolotl. Pero parecía un sueño lejano. Me sentí desconectado de la realidad.",
+  "Reflexioné sobre mi experiencia. Comprendí la conexión entre humanos y animales. Sentí una nueva empatía hacia los axolotls. Me comprometí a protegerlos.",
+  "Decidí compartir mi experiencia con otros. Escribí un libro sobre mi transformación. La gente se interesó en los axolotls. Se creó un movimiento para protegerlos.",
+  "Mi historia inspiró a futuras generaciones. Los axolotls fueron protegidos y respetados. Dejé un legado de amor y comprensión. Mi transformación no fue en vano.",
+  "FIN",
+  "CREDITOS\n Candela Di Lorenzo \n Agustin Tabbita \n Axolotl by Julio Cortazar"];
 
 let botonX = 430;
 let botonY = 420;
 let botonAncho = 200;
 let botonAlto = 50;
 let botonTexto = "Siguiente";
-let bax = 50;
+let bax = 20;
 let bat = "Atrás";
+let bbx = 225;
+let bbAncho = 150;
 let num = 0;
 let textoX = 120;
 let textoY = 150;
 let textoAncho = 400;
 let textoAlto = 400;
+let miFuente;
 
-function preload(){
-  for (let i=0; i<19; i++) {
-    imagenes[i] = loadImage("data/fondo"+i+".jpg")
+function preload() {
+  for (let i=0; i<21; i++) {
+    imagenes[i] = loadImage("data/fondo"+i+".jpg");
   }
-  imagenTexto0 = loadImage("data/texto0.png")
+  imagenTexto0 = loadImage("data/texto0.png");
 }
+
 function setup() {
   createCanvas(640, 480);
   estado = 0;
@@ -60,87 +65,94 @@ function setup() {
 function draw() {
   background(200)
   
-  
-  if (estado === 0) {
+    if (estado === 0) {
     cargarImagenEstado0(imagenes[0], imagenTexto0);
     //cargarImagensig(imagenes[0],0,0,640, 480, textos[0]);
-    dibujarBoton(botonX, botonY, botonAncho, botonAlto, botonTexto);
+    dibujarBoton(botonX, botonY, botonAncho, botonAlto, "Iniciar");
+    dibujarBoton(botonX, 350, botonAncho, botonAlto, "Créditos");
   } else if (estado === 1) {
-    cargarImagensig(imagenes[1],0,0,640, 480, textos[1]);
+    cargarImagensig(imagenes[1], 0, 0, 640, 480, textos[1]);
     dibujarBoton(botonX, botonY, botonAncho, botonAlto, botonTexto);
     dibujarBoton(bax, botonY, botonAncho, botonAlto, bat);
-  }else if (estado === 2) {
-    cargarImagensig(imagenes[2],0,0,640, 480, textos[2]);
+  } else if (estado === 2) {
+    cargarImagensig(imagenes[2], 0, 0, 640, 480, textos[2]);
     dibujarBoton(botonX, botonY, botonAncho, botonAlto, botonTexto);
     dibujarBoton(bax, botonY, botonAncho, botonAlto, bat);
-  }else if (estado === 3) {
-    cargarImagensig(imagenes[3],0,0,640, 480, textos[3]);
+  } else if (estado === 3) {
+    cargarImagensig(imagenes[3], 0, 0, 640, 480, textos[3]);
+    dibujarBoton(botonX, botonY, bbAncho, botonAlto, "Plantas");
+    dibujarBoton(bax, botonY, bbAncho, botonAlto, bat);
+    dibujarBoton(bbx, botonY, bbAncho, botonAlto, "Peces");
+  } else if (estado === 4) {
+    cargarImagensig(imagenes[4], 0, 0, 640, 480, textos[4]);
     dibujarBoton(botonX, botonY, botonAncho, botonAlto, botonTexto);
     dibujarBoton(bax, botonY, botonAncho, botonAlto, bat);
-  }else if (estado === 4) {
-    cargarImagensig(imagenes[4],0,0,640, 480, textos[4]);
+  } else if (estado === 5) {
+    cargarImagensig(imagenes[5], 0, 0, 640, 480, textos[5]);
     dibujarBoton(botonX, botonY, botonAncho, botonAlto, botonTexto);
     dibujarBoton(bax, botonY, botonAncho, botonAlto, bat);
-  }else if (estado === 5) {
-    cargarImagensig(imagenes[5],0,0,640, 480, textos[5]);
+  } else if (estado === 6) {
+    cargarImagensig(imagenes[6], 0, 0, 640, 480, textos[6]);
     dibujarBoton(botonX, botonY, botonAncho, botonAlto, botonTexto);
     dibujarBoton(bax, botonY, botonAncho, botonAlto, bat);
-  }else if (estado === 6) {
-    cargarImagensig(imagenes[6],0,0,640, 480, textos[6]);
+  } else if (estado === 7) {
+    cargarImagensig(imagenes[7], 0, 0, 640, 480, textos[7]);
     dibujarBoton(botonX, botonY, botonAncho, botonAlto, botonTexto);
     dibujarBoton(bax, botonY, botonAncho, botonAlto, bat);
-  }else if (estado === 7) {
-    cargarImagensig(imagenes[7],0,0,640, 480, textos[7]);
+  } else if (estado === 8) {
+    cargarImagensig(imagenes[8], 0, 0, 640, 480, textos[8]);
     dibujarBoton(botonX, botonY, botonAncho, botonAlto, botonTexto);
     dibujarBoton(bax, botonY, botonAncho, botonAlto, bat);
-  }else if (estado === 8) {
-    cargarImagensig(imagenes[8],0,0,640, 480, textos[8]);
+  } else if (estado === 9) {
+    cargarImagensig(imagenes[9], 0, 0, 640, 480, textos[9]);
+    dibujarBoton(botonX, botonY, 180, botonAlto, "Realidad");
+    dibujarBoton(bax, botonY, 180, botonAlto, bat);
+    dibujarBoton(bbx, botonY, 180, botonAlto, "Seguir axolotl");
+  } else if (estado === 10) {
+    cargarImagensig(imagenes[10], 0, 0, 640, 480, textos[10]);
     dibujarBoton(botonX, botonY, botonAncho, botonAlto, botonTexto);
     dibujarBoton(bax, botonY, botonAncho, botonAlto, bat);
-  }else if (estado === 9) {
-    cargarImagensig(imagenes[9],0,0,640, 480, textos[9]);
+  } else if (estado === 11) {
+    cargarImagensig(imagenes[11], 0, 0, 640, 480, textos[11]);
     dibujarBoton(botonX, botonY, botonAncho, botonAlto, botonTexto);
     dibujarBoton(bax, botonY, botonAncho, botonAlto, bat);
-  }else if (estado === 10) {
-    cargarImagensig(imagenes[10],0,0,640, 480, textos[10]);
+  } else if (estado === 12) {
+    cargarImagensig(imagenes[12], 0, 0, 640, 480, textos[12]);
     dibujarBoton(botonX, botonY, botonAncho, botonAlto, botonTexto);
     dibujarBoton(bax, botonY, botonAncho, botonAlto, bat);
-  }else if (estado === 11) {
-    cargarImagensig(imagenes[11],0,0,640, 480, textos[11]);
+  } else if (estado === 13) {
+    cargarImagensig(imagenes[13], 0, 0, 640, 480, textos[13]);
     dibujarBoton(botonX, botonY, botonAncho, botonAlto, botonTexto);
     dibujarBoton(bax, botonY, botonAncho, botonAlto, bat);
-  }else if (estado === 12) {
-    cargarImagensig(imagenes[12],0,0,640, 480, textos[12]);
+  } else if (estado === 14) {
+    cargarImagensig(imagenes[14], 0, 0, 640, 480, textos[14]);
+    dibujarBoton(botonX, botonY, botonAncho, botonAlto, "Inicio");
+    dibujarBoton(bax, botonY, botonAncho, botonAlto, bat);
+  } else if (estado === 15) {
+    cargarImagensig(imagenes[15], 0, 0, 640, 480, textos[15]);
+    dibujarBoton(botonX, botonY, 180, botonAlto, "Continuar");
+    dibujarBoton(bax, botonY, 180, botonAlto, bat);
+    dibujarBoton(bbx, botonY, 180, botonAlto, "Inicio");
+  } else if (estado === 16) {
+    cargarImagensig(imagenes[16], 0, 0, 640, 480, textos[16]);
     dibujarBoton(botonX, botonY, botonAncho, botonAlto, botonTexto);
     dibujarBoton(bax, botonY, botonAncho, botonAlto, bat);
-  }else if (estado === 13) {
-    cargarImagensig(imagenes[13],0,0,640, 480, textos[13]);
+  } else if (estado === 17) {
+    cargarImagensig(imagenes[17], 0, 0, 640, 480, textos[17]);
     dibujarBoton(botonX, botonY, botonAncho, botonAlto, botonTexto);
     dibujarBoton(bax, botonY, botonAncho, botonAlto, bat);
-  }else if (estado === 14) {
-    cargarImagensig(imagenes[14],0,0,640, 480, textos[14]);
+  } else if (estado === 18) {
+    cargarImagensig(imagenes[18], 0, 0, 640, 480, textos[18]);
     dibujarBoton(botonX, botonY, botonAncho, botonAlto, botonTexto);
     dibujarBoton(bax, botonY, botonAncho, botonAlto, bat);
-  }else if (estado === 15) {
-    cargarImagensig(imagenes[15],0,0,640, 480, textos[15]);
-    dibujarBoton(botonX, botonY, botonAncho, botonAlto, botonTexto);
-    dibujarBoton(bax, botonY, botonAncho, botonAlto, bat);
-  }else if (estado === 16) {
-    cargarImagensig(imagenes[16],0,0,640, 480, textos[16]);
-    dibujarBoton(botonX, botonY, botonAncho, botonAlto, botonTexto);
-    dibujarBoton(bax, botonY, botonAncho, botonAlto, bat);
-  }else if (estado === 17) {
-    cargarImagensig(imagenes[17],0,0,640, 480, textos[17]);
-    dibujarBoton(botonX, botonY, botonAncho, botonAlto, botonTexto);
-    dibujarBoton(bax, botonY, botonAncho, botonAlto, bat);
-  }else if (estado === 18) {
-    cargarImagensig(imagenes[18],0,0,640, 480, textos[18]);
-    dibujarBoton(botonX, botonY, botonAncho, botonAlto, botonTexto);
-    dibujarBoton(bax, botonY, botonAncho, botonAlto, bat);
-  }else if (estado === 19) {
-    cargarImagensig(imagenes[19],0,0,640, 480, textos[19]);
-    dibujarBoton(botonX, botonY, botonAncho, botonAlto, botonTexto);
-    dibujarBoton(bax, botonY, botonAncho, botonAlto, bat);
+  } else if (estado === 19) {
+    cargarImagensig(imagenes[19], 0, 0, 640, 480, textos[19]);
+    dibujarBoton(botonX, botonY, 180, botonAlto, "Ver otro final");
+    dibujarBoton(bax, botonY, 180, botonAlto, bat);
+    dibujarBoton(bbx, botonY, 180, botonAlto, "Inicio");
+  } else if (estado === 20) {
+    cargarImagensig(imagenes[20], 0, 0, 640, 480, textos[20]);
+    dibujarBoton(botonX, botonY, botonAncho, botonAlto, "Inicio");
   }
 }
 
@@ -148,6 +160,9 @@ function mousePressed() {
   if (estado === 0) {
     if (detectarBoton(botonX, botonY, botonAncho, botonAlto)) {
       estado = 1;
+    }
+    if (detectarBoton(botonX, 350, botonAncho, botonAlto)) {
+      estado = 20;
     }
   } else if (estado === 1) {
     if (detectarBoton(botonX, botonY, botonAncho, botonAlto)) {
@@ -164,16 +179,140 @@ function mousePressed() {
       estado = 1;
     }
   } else if (estado === 3) {
-    if (detectarBoton(botonX, botonY, botonAncho, botonAlto)) {
+    if (detectarBoton(botonX, botonY, bbAncho, botonAlto)) {
       estado = 4;
     }
-    if (detectarBoton(bax, botonY, botonAncho, botonAlto)) {
+    if (detectarBoton(bax, botonY, bbAncho, botonAlto)) {
       estado = 2;
-      }  else if (estado === 4) {
+    }
+    if (detectarBoton(bbx, botonY, bbAncho, botonAlto)) {
+      estado = 5;
+    }
+  } else if (estado === 4) {
+    if (detectarBoton(botonX, botonY, botonAncho, botonAlto)) {
+      estado = 6;
+    }
     if (detectarBoton(bax, botonY, botonAncho, botonAlto)) {
       estado = 3;
     }
-      }
+  } else if (estado === 5) {
+    if (detectarBoton(botonX, botonY, botonAncho, botonAlto)) {
+      estado = 6;
+    }
+    if (detectarBoton(bax, botonY, botonAncho, botonAlto)) {
+      estado = 3;
+    }
+  } else if (estado === 6) {
+    if (detectarBoton(botonX, botonY, botonAncho, botonAlto)) {
+      estado = 7;
+    }
+    if (detectarBoton(bax, botonY, botonAncho, botonAlto)) {
+      estado = 5;
+    }
+  } else if (estado === 7) {
+    if (detectarBoton(botonX, botonY, botonAncho, botonAlto)) {
+      estado = 8;
+    }
+    if (detectarBoton(bax, botonY, botonAncho, botonAlto)) {
+      estado = 6;
+    }
+  } else if (estado === 8) {
+    if (detectarBoton(botonX, botonY, botonAncho, botonAlto)) {
+      estado = 9;
+    }
+    if (detectarBoton(bax, botonY, botonAncho, botonAlto)) {
+      estado = 7;
+    }
+  } else if (estado === 9) {
+    if (detectarBoton(botonX, botonY, bbAncho, botonAlto)) {
+      estado = 10;
+    }
+    if (detectarBoton(bax, botonY, bbAncho, botonAlto)) {
+      estado = 8;
+    }
+    if (detectarBoton(bbx, botonY, bbAncho, botonAlto)) {
+      estado = 11;
+    }
+  } else if (estado === 10) {
+    if (detectarBoton(botonX, botonY, botonAncho, botonAlto)) {
+      estado = 15;
+    }
+    if (detectarBoton(bax, botonY, botonAncho, botonAlto)) {
+      estado = 9;
+    }
+  } else if (estado === 15) {
+    if (detectarBoton(botonX, botonY, bbAncho, botonAlto)) {
+      estado = 16;
+    }
+    if (detectarBoton(bax, botonY, bbAncho, botonAlto)) {
+      estado = 9;
+    }
+    if (detectarBoton(bbx, botonY, bbAncho, botonAlto)) {
+      estado = 0;
+    }
+  } else if (estado === 16) {
+    if (detectarBoton(botonX, botonY, botonAncho, botonAlto)) {
+      estado = 17;
+    }
+    if (detectarBoton(bax, botonY, botonAncho, botonAlto)) {
+      estado = 15;
+    }
+  } else if (estado === 17) {
+    if (detectarBoton(botonX, botonY, botonAncho, botonAlto)) {
+      estado = 18;
+    }
+    if (detectarBoton(bax, botonY, botonAncho, botonAlto)) {
+      estado = 16;
+    }
+  } else if (estado === 18) {
+    if (detectarBoton(botonX, botonY, botonAncho, botonAlto)) {
+      estado = 19;
+    }
+    if (detectarBoton(bax, botonY, botonAncho, botonAlto)) {
+      estado = 17;
+    }
+  } else if (estado === 19) {
+    if (detectarBoton(botonX, botonY, bbAncho, botonAlto)) {
+      estado = 9;
+    }
+    if (detectarBoton(bax, botonY, bbAncho, botonAlto)) {
+      estado = 18;
+    }
+    if (detectarBoton(bbx, botonY, bbAncho, botonAlto)) {
+      estado = 0;
+    }
+  } else if (estado === 11) {
+    if (detectarBoton(botonX, botonY, botonAncho, botonAlto)) {
+      estado = 12;
+    }
+    if (detectarBoton(bax, botonY, botonAncho, botonAlto)) {
+      estado = 9;
+    }
+  } else if (estado === 12) {
+    if (detectarBoton(botonX, botonY, botonAncho, botonAlto)) {
+      estado = 13;
+    }
+    if (detectarBoton(bax, botonY, botonAncho, botonAlto)) {
+      estado = 11;
+    }
+  } else if (estado === 13) {
+    if (detectarBoton(botonX, botonY, botonAncho, botonAlto)) {
+      estado = 14;
+    }
+    if (detectarBoton(bax, botonY, botonAncho, botonAlto)) {
+      estado = 12;
+    }
+  } else if (estado === 14) {
+    if (detectarBoton(botonX, botonY, botonAncho, botonAlto)) {
+      estado = 0;
+    }
+    if (detectarBoton(bax, botonY, botonAncho, botonAlto)) {
+      estado = 13;
+    }
+  } else if (estado === 20) {
+    if (detectarBoton(botonX, botonY, botonAncho, botonAlto)) {
+      estado = 0;
+    }
   }
 }
 
@@ -192,7 +331,7 @@ function dibujarBoton(x, y, an, al, texto) {
     stroke(0, 180, 180);
     strokeWeight(4);
   }
-  rect(x, y, an, al, 8, 8, 8, 8);
+  rect(x, y, an, al, 8);
   fill(255);
   noStroke();
   textSize(25);
@@ -205,11 +344,11 @@ function detectarBoton(x, y, an, al) {
 }
 
 function cargarImagensig(imag, x, y, ancho, alto, texto) {
-   image(imag, x, y, ancho, alto);
-   fill(255,255,255,200)
-   noStroke()
-   rect(textoX, textoY, textoAncho, textoAlto, 10)
-   fill(0);
-   textSize(16);
-   text(texto, textoX, textoY, textoAncho, textoAlto);
+  image(imag, x, y, ancho, alto);
+  fill(255, 255, 255, 200);
+    noStroke();
+    rect(120, 295, 400, 110, 10);
+    fill(0);
+  textSize(16);
+  text(texto, textoX, textoY, textoAncho, textoAlto);
 }
